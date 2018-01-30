a76518e14a442a468ef490c0f8d6da654336f862
ABQJuan <friedrichjohn@hotmail.com>
2018-01-29 23:00:39 -0700

    Cleanup stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 
-let defaulTextColor = '#fff';
 let defaultStyle = {
-  color: defaulTextColor
+  color: '#fff'
 
 };
 
@@ -54,7 +53,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 style={{ color: defaulTextColor }}>Title</h1>
+        <h1 style={{...defaultStyle, 'font-size':'54px'}}>Title</h1>
         <Aggregate />
         <Aggregate />
         <Filter />
